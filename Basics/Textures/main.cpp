2d089ae27dcc58658d0ea1fbc558295399da8eef
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2019-12-07 01:03:12 +0300

    Textures: finished the main task

--- Basics/Textures/main.cpp
+++ Basics/Textures/main.cpp
@@ -41,33 +41,75 @@ int main() {
 		return -1;
 	}
 
+	// Set up stb_image
+	stbi_set_flip_vertically_on_load(true);
+
 	// Create shader object (using Shader class)
 	Shader shader("shader.vert", "shader.frag");
 
-	// Create texture
-	unsigned int texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	//// Create texture
+	//unsigned int texture;
+	//glGenTextures(1, &texture);
+	//glActiveTexture(GL_TEXTURE0);
+	//glBindTexture(GL_TEXTURE_2D, texture);
 
-	// Setting wrapping and filtering properties of the current texture
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//// Setting wrapping and filtering properties of the current texture
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	// Generating texture and filling it
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
-	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
+	//// Generating texture and filling it
+	//int width, height, nrChannels;
+	//unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	//if (data) {
+	//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	//	glGenerateMipmap(GL_TEXTURE_2D);
+	//}
+	//else {
+	//	std::cout << "Failed to load texture" << std::endl;
+	//}
+
+	//// Free image's memory
+	//stbi_image_free(data);
+
+	// Create textures
+	unsigned int textures[2];
+	glGenTextures(2, textures);
+
+	// Textures' filepaths
+	const char* textureNames[] = { "container.jpg", "awesomeface.png" };
+
+	for (auto i = 0; i < 2; ++i) {
+		glActiveTexture(GL_TEXTURE0 + i);
+		glBindTexture(GL_TEXTURE_2D, textures[i]);
+	
+		// Setting wrapping and filtering properties for every texture
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// Generating textures and filling them
+		int width, height, nrChannels;
+		unsigned char* data = stbi_load(textureNames[i], &width, &height, &nrChannels, 0);
+		
+		if (data) {
+			// A dirty hack for the purpose of this tutorial
+			// It is here because one image is .jpg (no alpha) the other is .png
+			auto imageMode = i ? GL_RGBA : GL_RGB;
+
+			// Load texture, generate mipmap
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, imageMode, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else {
+			std::cout << "Failed to load texture" << std::endl;
+		}
+	
+		// Free images' memory
+		stbi_image_free(data);
 	}
-	else {
-		std::cout << "Failed to load texture" << std::endl;
-	}
-
-	// Free image's memory
-	stbi_image_free(data);
 
 	// Rectangle's vertices
 	float vertices[] = {
@@ -109,12 +151,15 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+
+
 	// Enable wireframe mode
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	// Get uniform attributes' locations (task with uniform)
+	// Set uniform attributes' values
 	shader.use();
-	//shader.setFloat("triangleOffset", 0.5);
+	shader.setInt("texture1", 0);
+	shader.setInt("texture2", 1);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
@@ -128,7 +173,8 @@ int main() {
 		shader.use();
 
 		// Binding texture
-		glBindTexture(GL_TEXTURE_2D, texture);
+		//glBindTexture(GL_TEXTURE_2D, textures[0]);
+		glActiveTexture(1);
 
 		// Binding VAO
 		glBindVertexArray(VAO);
--- Basics/Textures/shader.vert
+++ Basics/Textures/shader.vert
@@ -6,8 +6,6 @@ layout (location = 2) in vec2 aTexCoord;
 out vec3 ourColor;
 out vec2 texCoord;
 
-uniform float triangleOffset;
-
 void main()
 {
 	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
