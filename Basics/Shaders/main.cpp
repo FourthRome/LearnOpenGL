cde457e217ac127008705dd3f601f902208f3850
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2019-12-06 10:43:41 +0300

    Multicolor triangle task

--- Basics/Shaders/main.cpp
+++ Basics/Shaders/main.cpp
@@ -3,24 +3,26 @@
 #include <GLFW/glfw3.h>
 
 const char* vertexShaderSource = "#version 330 core\n\
-layout(location = 0) in vec3 aPos;\n\
+layout (location = 0) in vec3 aPos;\n\
+layout (location = 1) in vec3 aColor;\n\
 \n\
-out vec4 vertexColor;\n\
+out vec3 ourColor;\n\
 \n\
 void main()\n\
 {\n\
 	gl_Position = vec4(aPos, 1.0);\n\
-	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n\
+	ourColor = aColor;\n\
 }\0";
 
 const char* fragmentShaderSource = "#version 330 core\n\
 out vec4 FragColor;\n\
 \n\
-uniform vec4 ourColor;\n\
+//uniform vec4 ourColor;\n\
+in vec3 ourColor;\n\
 \n\
 void main()\n\
 {\n\
-	FragColor = ourColor;\n\
+	FragColor = vec4(ourColor, 1.0);\n\
 }\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -108,10 +110,17 @@ int main() {
 	glDeleteShader(fragmentShader);
 
 	// Triangle's vertices
+	//float vertices[] = {
+	//	-0.5f, -0.5f, 0.0f,
+	//	0.5f, -0.5f, 0.0f,
+	//	0.0f, 0.5f, 0.0f
+	//};
+
+	// Triangle's vertices (with colors)
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 	};
 
 	// Create vertex array object
@@ -126,14 +135,16 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	// Interpretation for vertex data
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)NULL);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) NULL);
 	glEnableVertexAttribArray(0);
-
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	
 	// Enable wireframe mode
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	// Get uniform attributes' locations
-	int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+	// Get uniform attributes' locations (task with uniform)
+	//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 
 
 	// Main loop
@@ -146,9 +157,11 @@ int main() {
 
 		// Activate shader program
 		glUseProgram(shaderProgram);
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		// Fill triangle with green (task with uniform)
+		//float timeValue = glfwGetTime();
+		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
 		// Binding VAO
 		glBindVertexArray(VAO);
